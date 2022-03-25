59ae9a0bea9d73bb7e67fdfea868ffb2df0821ab
Nakul Singh <94111518+codernakul@users.noreply.github.com>
2022-03-26 04:08:35 +0530

    updated O1
    added Eraser function in Paint Tool

--- OPENCV/Q1_Paint_Tool/paint.py
+++ OPENCV/Q1_Paint_Tool/paint.py
@@ -4,7 +4,7 @@ import numpy as np
 draw = False
 window_name = "Canvas"
 
-img1 = np.zeros((609, 611, 3), np.uint8)
+img1 = np.zeros((695, 615, 3), np.uint8)
 img1[:] = 225, 225, 225
 
 
@@ -35,14 +35,33 @@ def draw_circle(event, x, y, flags, param):
                    -1)
 
 
+def eraser(event, x, y, flags, param):
+    global draw, window_name, img1
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        draw = True
+
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if draw:
+            cv2.circle(img1, (x, y), cv2.getTrackbarPos("Eraser Size", 'paint'),
+                       (225,225,225), -1)
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        draw = False
+        cv2.circle(img1, (x, y), cv2.getTrackbarPos("Eraser", 'paint'),
+                   (225,225,225), -1)
+
+
 # Create a black paint, a window
-img = np.zeros((310, 512, 3), np.uint8)
+img = np.zeros((250, 512, 3), np.uint8)
 cv2.namedWindow('paint')
 # create trackbars for color change
 cv2.createTrackbar('R', 'paint', 0, 255, nothing)
 cv2.createTrackbar('G', 'paint', 0, 255, nothing)
 cv2.createTrackbar('B', 'paint', 0, 255, nothing)
 cv2.createTrackbar("Brush Size", 'paint', 1, 8, nothing)
+cv2.createTrackbar("Eraser", 'paint', 0, 1, nothing)
+cv2.createTrackbar("Eraser Size", 'paint', 1, 8, nothing)
 
 # create switch for ON/OFF functionality
 switch = '0 : OFF \n1 : ON'
@@ -59,13 +78,19 @@ while 1:
     g = cv2.getTrackbarPos('G', 'paint')
     b = cv2.getTrackbarPos('B', 'paint')
     s = cv2.getTrackbarPos(switch, 'paint')
+    er = cv2.getTrackbarPos("Eraser", 'paint')
+
     if s == 0:
         img[:] = 0
     else:
+
         cv2.namedWindow(window_name)
 
         cv2.imshow(window_name, img1)
         img[:] = [b, g, r]
         cv2.setMouseCallback(window_name, draw_circle)
+
+        if er == 1:
+            cv2.setMouseCallback(window_name, eraser)
 
 cv2.destroyAllWindows()
